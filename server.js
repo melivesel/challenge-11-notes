deedbd773836217724052ebf6bb919ba2a7e0c69
Melissa Bentley <melivesel@gmail.com>
2024-06-19 20:19:17 -0500

    updated folders to try and get render to work

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const path = require('path');
-const noteData = require('../db/db.json');
+const noteData = require('./db/db.json');
 const fs = require('fs');
 const { v4: uuidv4 } = require('uuid');
-const filePath = '../db/db.json';
+const filePath = './db/db.json';
 const PORT = 3001;
 
 const app = express();
