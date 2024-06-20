5c085eaaf9d7272f34f7ab8b23cdb32d68223680
Melissa Bentley <melivesel@gmail.com>
2024-06-19 20:29:00 -0500

    repaired path to notes.html

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.get('/', (req, res) => {
 });
 
 app.get('/notes', (req, res) => {
-  res.sendFile(path.join(__dirname, 'notes.html'));
+  res.sendFile(path.join(__dirname, '/public/notes.html'));
 });
 
 app.get('/api/notes', (req, res) => res.json(noteData));
